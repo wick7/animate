26cb87935549644491fe5833c8c01be661b312e2
Craig Wickersham <Wick@Craigs-MacBook-Pro-2.local>
2017-04-15 19:23:07 -0700

    cleaned up

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,116 +1,102 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Joe's Lumber Yard</title> 
+    <title>Joe's Lumber Yard</title> 
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Ewert" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=BioRhyme|Ewert" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body, .container {
-                height: auto;
-                width: auto;
-                outline: 0px;
-                border: 0px;
-                padding: 0px;
-                margin: 0px;
-                
-                
-                background-color: ;
-            }
+<style>
 
-            
-         
+html, body, .container {
+    height: auto;
+    width: auto;
+    outline: 0px;
+    border: 0px;
+    padding: 0px;
+    margin: 0px;
+    background-color: ;
+}
 
-            ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-                font-family: 'Open Sans', sans-serif;
-                
-            }
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    font-family: 'Open Sans', sans-serif;
+}
 
-            li {
-                float: right;
+li {
+    float: right;
+}
 
-            }
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 12px 13px;
+    text-decoration: none;
+    margin-top: .5em;
+}
 
-            li a {
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 12px 13px;
-                text-decoration: none;
-                margin-top: .5em;
-            }
+li a:hover:not(.active) {
+    background-color: rgb(2, 23, 255);
+}
 
-            li a:hover:not(.active) {
-                background-color: rgb(2, 23, 255);
-            }
+@media(min-width: 780px) {
+    li {
+        float: right;
+    }
 
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 24px 26px;
+        text-decoration: none;
+    }
 
-            @media(min-width: 780px) {
-              li {
-                float: right;
-            }
+    li a:hover:not(.active) {
+        background-color: rgb(2, 23, 255);
+    }
+}
 
-            li a {
-                display: block;
-                color: white;
-                text-align: center;
-                padding: 24px 26px;
-                text-decoration: none;
-            }
+.active {
+    background-color: #111;
+}
 
-            li a:hover:not(.active) {
-                background-color: rgb(2, 23, 255);
-            }
-            }
+.logo {
+    display: block;
+    float: left;
+    font-family: 'Ewert', cursive;
+    background-color: ;
+    vertical-align: middle;
+    color: white;
+}
 
-            .active {
-                background-color: #111;
-            }
+.logo_inner h1 {
+    font-size: 3em;
+    margin-top: .1em;
+    margin-left: 0em;
+}
 
-            .logo {
-                display: block;
-                float: left;
-                font-family: 'Ewert', cursive;
-                background-color: ;
-                vertical-align: middle;
-                color: white;
-            }
+</style>
 
-            .logo_inner h1 {
-                font-size: 3em;
-               margin-top: .1em;
-               margin-left: 0em;
-            }
-
-
-            
-        </style>
-    </head>
+</head>
     
-            
-
-        <div class="container">
-            
-
-            
+    <div class="container">        
         @yield('content')
         @yield('footer')
-        
+    </div>
 
-        </div>
 </html>
--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -2,79 +2,73 @@
 
 @section('content')
 
-        <title>Joe's Lumber Yard</title>
+<title>Joe's Lumber Yard</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            
-
-            .header {
-                height: 100%;
-                width: 100%;
-                background-color: purple;
-            }
-
-            .header_table {
-                display: table;
-            }
-
-            .table {
-                display: table-cell;
-                vertical-align: middle;
-                height: 100%;
-                width: 100%;
-            }
-
-            .table h1 {
-                text-align: center;
-                color: white;
-                font-family: 'Raleway', sans-serif;
-            }
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 
 
-            .tablet {
-                height: 280px;
-            }
+<!-- Styles -->
+<style>
 
-            input {
-                border: 1px;
-                background-color: yellow;
-                display: inline-block;
-                vertical-align: middle;
-                text-align: center;
-                height: 100%;
-                
-            }
-            
-        </style>
 
-        <div class="container">
+    .header {
+        height: 100%;
+        width: 100%;
+        background-color: gray;
+    }
 
-            <div class="header header_table">
+    .header_table {
+        display: table;
+    }
 
-                
-                <ul>
-              
-              <li><a href="{{ url('about') }}">About</a></li>
-              <li><a href="{{ url('/') }}">Home</a></li>
-            </ul> 
-                    <h1>Attribution</h1>
-                    
-                    <div>Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-                        <div>Icons made by <a href="http://www.flaticon.com/authors/nikita-golubev" title="Nikita Golubev">Nikita Golubev</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-                        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-                        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-                </div>
-                
-            </div>
-            <div class="animate">
-                
-            </div>
-        </div>
-        
+    .table {
+        display: table-cell;
+        vertical-align: middle;
+        height: 100%;
+        width: 100%;
+    }
+
+    .table h1 {
+        text-align: center;
+        color: white;
+        font-family: 'Raleway', sans-serif;
+    }
+
+
+    .tablet {
+        height: 280px;
+    }
+
+    input {
+        border: 1px;
+        background-color: yellow;
+        display: inline-block;
+        vertical-align: middle;
+        text-align: center;
+        height: 100%;
+
+    }
+
+</style>
+
+<div class="container">
+    <div class="header header_table">
+    <ul>
+        <li><a href="{{ url('about') }}">About</a></li>
+        <li><a href="{{ url('/') }}">Home</a></li>
+    </ul> 
+        <h1>Attribution</h1>
+
+        <div>Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        <div>Icons made by <a href="http://www.flaticon.com/authors/nikita-golubev" title="Nikita Golubev">Nikita Golubev</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    </div>
+</div>
+    <div class="animate"></div>
+</div>
+
 @endsection
